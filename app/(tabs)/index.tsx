b8a93ad80b77a42404037689af1b0a75f572d7be
Michal <s24407@pjwstk.edu.pl>
2024-07-10 12:49:59 +0200

    Setup NativeWind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,5 @@ import { View, Text } from "react-native"
 import React from "react"
 
 export default function FeedScreen() {
-  return (
-    <View>
-      <Text>feed</Text>
-    </View>
-  )
+  return <Text className=" text-5xl">feed</Text>
 }
